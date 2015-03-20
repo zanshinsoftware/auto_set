e41ad7034ee3e4868c70fcaa328fe263c248800b
Felipe Diesel <felipediesel@gmail.com>
2015-03-20 11:08:52 -0300

    Remove code that has been moved.

--- lib/auto_set/active_record.rb
+++ lib/auto_set/active_record.rb
@@ -2,15 +2,6 @@ module AutoSet
   module ActiveRecord
     extend ActiveSupport::Concern
 
-    private
-
-    def auto_set_parent_by_code(column, code)
-      if self.send(code).present?
-        reflection = self.class.reflections[column.to_s]
-        reflection.klass.where(code: self.send(code)).first if reflection.present?
-      end
-    end
-
     module ClassMethods
       def auto_set(column, parents, options = {})
         before = (parents.is_a?(Hash) ? parents : options).delete(:before)
